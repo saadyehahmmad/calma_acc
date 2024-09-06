5c67aeb50416a640da7966eac5b15b8205de14a3
saadyehahmmad <129695973+saadyehahmmad@users.noreply.github.com>
2024-09-06 19:46:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from tmtn import create_app, socketio
+from tmtn import create_app
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     try:
-        socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+        app.run( host='0.0.0.0', port=5353, debug=True)
     except Exception as e:
         print(f"Error running the app: {e}")
